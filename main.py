b422dfb1bfa5c72e91d8c437ada07f6c006702c0
nc30 <nc30@users.noreply.github.com>
2018-11-21 02:50:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,50 +11,66 @@ import sys
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import json
 
-CHECK_SPAN = 1800
+CHECK_SPAN = 10
 THING_NAME = 'arare'
 ENDPOINT = 'a3gt2cb172okvl-ats.iot.ap-northeast-1.amazonaws.com'
 ROOTCA = '/home/pi/AmazonRootCA1.pem'
 PRIVATE = '/home/pi/ebf86ded56-private.pem.key'
 CERT = '/home/pi/ebf86ded56-certificate.pem.crt'
 
-client = AWSIoTMQTTClient(THING_NAME)
-client.configureEndpoint(ENDPOINT, 8883)
-client.configureCredentials(ROOTCA, PRIVATE, CERT)
 
-client.configureAutoReconnectBackoffTime(1, 32, 20)
-client.configureOfflinePublishQueueing(-1)
-client.configureDrainingFrequency(2)
-client.configureConnectDisconnectTimeout(300)
-client.configureMQTTOperationTimeout(5)
+def cb(client, userdata, message):
+    r = json.loads(message.payload.decode('utf-8'))
 
-client.connect(60)
-
-while True:
     try:
-        # Shadowアップデートに必要な構造の作成
-        # https://docs.aws.amazon.com/ja_jp/iot/latest/developerguide/device-shadow-mqtt.html#update-pub-sub-topic
-        shadow = {
-            "state": {
-                "reported": {
-                    "date": time.time() 
+        global CHECK_SPAN
+        CHECK_SPAN = int(r['state']['check_span'])
+    except KeyError:
+        pass
+
+if __name__ == '__main__':
+    from logging import StreamHandler
+    logger.addHandler(StreamHandler(stream=sys.stdout))
+
+    client.connect(60)
+    client.subscribe('$aws/things/'+THING_NAME+'/shadow/update/delta', 1, cb)
+
+    client = AWSIoTMQTTClient(THING_NAME)
+    client.configureEndpoint(ENDPOINT, 8883)
+    client.configureCredentials(ROOTCA, PRIVATE, CERT)
+
+    client.configureAutoReconnectBackoffTime(1, 32, 20)
+    client.configureOfflinePublishQueueing(-1)
+    client.configureDrainingFrequency(2)
+    client.configureConnectDisconnectTimeout(300)
+    client.configureMQTTOperationTimeout(5)
+
+
+    while True:
+        try:
+            # Shadowアップデートに必要な構造の作成
+            # https://docs.aws.amazon.com/ja_jp/iot/latest/developerguide/device-shadow-mqtt.html#update-pub-sub-topic
+            shadow = {
+                "state": {
+                    "reported": {
+                        "date": time.time() 
+                    }
                 }
             }
-        }
 
 
-        # Shadowのアップデートを行う
-        # jsonについてはこちら
-        # https://docs.python.jp/3/library/json.html
-        client.publish('$aws/things/'+THING_NAME+'/shadow/update', json.dumps(shadow), 1)
+            # Shadowのアップデートを行う
+            # jsonについてはこちら
+            # https://docs.python.jp/3/library/json.html
+            client.publish('$aws/things/'+THING_NAME+'/shadow/update', json.dumps(shadow), 1)
 
-    except IOError:
-        # this is connection error to enviro phat
+        except IOError:
+            # this is connection error to enviro phat
+            time.sleep(CHECK_SPAN)
+            continue
+
+        except Exception as e:
+            logger.exception(e)
+
+        logger.debug(time.time())
         time.sleep(CHECK_SPAN)
-        continue
-
-    except Exception as e:
-        logger.exception(e)
-
-    print(time.time())
-    time.sleep(CHECK_SPAN)
